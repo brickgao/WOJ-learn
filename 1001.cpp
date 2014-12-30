b1ccfb02a8e155a2e7e6bd7cc1c23faa701ee03d
brickgao <brickgao@gmail.com>
2014-12-30 17:18:10 +0800

    add 1001

---
+++ 1001.cpp
@@ -0,0 +1,23 @@
+//By Brickgao
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#define out(v) cerr << #v << ": " << (v) << endl
+#define SZ(v) ((int)(v).size())
+const int maxint = -1u>>1;
+template <class T> bool get_max(T& a, const T &b) {return b > a? a = b, 1: 0;}
+template <class T> bool get_min(T& a, const T &b) {return b < a? a = b, 1: 0;}
+
+int main() {
+    int a, b;
+    while(scanf("%d%d", &a, &b) != EOF) {
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
+
