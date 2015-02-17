615e82b207206ac668c67962efba99e902cf16c8
brickgao <brickgao@gmail.com>
2015-02-18 00:40:58 +0800

    add 1413

---
+++ 1413.cpp
@@ -0,0 +1,38 @@
+//By Brickgao
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <map>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+#include <list>
+#include <iomanip>
+#define out(v) cout << #v << " " << v << endl
+#define SZ(v) ((int)(v).size())
+#define PI acos(-1.0)
+using namespace std;
+typedef long long lint;
+const int inf = 1e9 + 7;
+const double eps = 1e-8;
+inline int sgn(const double x) {return (x > eps) - (x < -eps);}
+template <class T> T _abs(T x) {if(x < 0) x = -x; return x;}
+template <class T> bool get_max(T &a, const T b) {return a < b ? a = b, true : false;}
+template <class T> bool get_min(T &a, const T b) {return a > b ? a = b, true : false;}
+template <class T> void _swap(T &a, T &b) {T t = a; a = b; b = t;}
+
+int main() {
+    int t;
+    while(scanf("%d", &t) != EOF) {
+        for(int T = 0; T < t; ++ T) {
+            double a, b, h;
+            cin >> a >> b >> h;
+            printf("%.2lf\n", h * b / a);
+        }
+    }
+    return 0;
+}
